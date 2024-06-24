66b0d2a440d67af414babc64f1cea41185f69007
Himanshu Chaurasia <coderhimanshu09@gmail.com>
2024-06-24 18:13:37 +0530

    Add Designation and Department model

--- Authentication/models.py
+++ Authentication/models.py
@@ -2,12 +2,10 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 from Master.models import TimeStamp
-
-
 # Create your models here.
 
 class CompanyType(TimeStamp):
-    title = models.CharField(_('Company Type'), max_length=255, unique=True)
+    title = models.CharField(_('Company Type'),max_length=255, unique=True)
 
     def __str__(self):
         return self.title
--- General/models.py
+++ General/models.py
@@ -4,17 +4,42 @@ from Master.models import TimeStamp
 
 
 class Role(TimeStamp):
-    title = models.CharField(_('Role'), max_length=50, unique=True)
+    name = models.CharField(_('Role'), max_length=50, unique=True)
 
     def __str__(self):
-        return self.title
+        return self.name
 
     def __repr__(self):
-        return f"<Role(title={self.title})>"
+        return f"<Role(title={self.name})>"
 
     class Meta:
         verbose_name = _('Role')
         verbose_name_plural = _('Roles')
-    
-    
-    
+
+
+class Designation(TimeStamp):
+    name = models.CharField(_('Designation'), max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Designation(title={self.name})>"
+
+    class Meta:
+        verbose_name = _('Designation')
+        verbose_name_plural = _('Designations')
+
+
+class Department(TimeStamp):
+    name = models.CharField(_('Department'), max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Department(title={self.name})>"
+
+    class Meta:
+        verbose_name = _('Department')
+        verbose_name_plural = _('Departments')
