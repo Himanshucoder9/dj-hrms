58042341785444dbfad9b3e737de6993bbd38c33
Himanshu Chaurasia <coderhimanshu09@gmail.com>
2024-06-24 18:15:12 +0530

    Changes in Company model

--- Authentication/models.py
+++ Authentication/models.py
@@ -2,16 +2,18 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 from Master.models import TimeStamp
+
+
 # Create your models here.
 
 class CompanyType(TimeStamp):
-    title = models.CharField(_('Company Type'),max_length=255, unique=True)
+    name = models.CharField(_('Company Type'), max_length=255, unique=True)
 
     def __str__(self):
-        return self.title
+        return self.name
 
     def __repr__(self):
-        return f"<CompanyType(title={self.title})>"
+        return f"<CompanyType(title={self.name})>"
 
     class Meta:
         verbose_name = _('Company Type')
@@ -21,14 +23,15 @@ class CompanyType(TimeStamp):
 class Company(TimeStamp):
     company_code = models.CharField(_("Company Code"), max_length=6, unique=True)
     name = models.CharField(_('Company Name'), max_length=255, unique=True)
-    company_type = models.ForeignKey(_('Company Type'), CompanyType, on_delete=models.SET_NULL, blank=True, null=True)
+    company_type = models.ForeignKey(CompanyType, on_delete=models.SET_NULL, blank=True, null=True,
+                                     verbose_name=_('Company Type'))
     registered_office_address = models.TextField(_('Registered Office Address'))
 
     def __str__(self):
         return f"{self.company_code} - {self.name}"
 
     def __repr__(self):
-        return f"<CompanyType(title={self.title})>"
+        return f"<Company(name={self.name})>"
 
     class Meta:
         verbose_name = _('Company')
