c9ed88abfcc48195b498404727ccc2c58dcbaa2e
Himanshu Chaurasia <coderhimanshu09@gmail.com>
2024-06-24 17:32:57 +0530

    Add Expense model

--- Authentication/models.py
+++ Authentication/models.py
@@ -2,17 +2,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 from Master.models import TimeStamp
+
+
 # Create your models here.
 
 class CompanyType(TimeStamp):
-    title = models.CharField(_('Company Type'),max_length=255, unique=True)
+    title = models.CharField(_('Company Type'), max_length=255, unique=True)
 
     def __str__(self):
         return self.title
-    
+
     def __repr__(self):
         return f"<CompanyType(title={self.title})>"
-    
+
     class Meta:
         verbose_name = _('Company Type')
         verbose_name_plural = _('Company Types')
@@ -23,13 +25,13 @@ class Company(TimeStamp):
     name = models.CharField(_('Company Name'), max_length=255, unique=True)
     company_type = models.ForeignKey(_('Company Type'), CompanyType, on_delete=models.SET_NULL, blank=True, null=True)
     registered_office_address = models.TextField(_('Registered Office Address'))
-   
+
     def __str__(self):
         return f"{self.company_code} - {self.name}"
-    
+
     def __repr__(self):
         return f"<CompanyType(title={self.title})>"
-    
+
     class Meta:
         verbose_name = _('Company')
         verbose_name_plural = _('Companies')
--- Expense/models.py
+++ Expense/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from Master.models import TimeStamp
+from Master.myvalidator import alphanumeric, alphabet
+from imagekit.models import ProcessedImageField
+
 
 # Create your models here.
+
+class Head(TimeStamp):
+    company = models.ForeignKey(_("Company"), 'Authentication.Company', on_delete=models.CASCADE)
+    expense_head = models.CharField(_("expense head"), max_length=250, validators=[alphanumeric('Expense head')])
+    note = models.TextField(_("note"), blank=True, null=True)
+
+    def __str__(self):
+        return self.expense_head
+
+    def __repr__(self):
+        return f"<Head(expense_head={self.expense_head})>"
+
+    class Meta:
+        verbose_name = _("Head")
+        verbose_name_plural = _("Heads")
+
+
+class Expense(TimeStamp):
+    company = models.ForeignKey(_("Company"), 'Authentication.Company', on_delete=models.CASCADE)
+    expense_head = models.ForeignKey(Head, on_delete=models.CASCADE, verbose_name="Expense head")
+    name = models.CharField(_("name"), max_length=250, validators=[alphabet('Name')])
+    invoice_no = models.CharField(_("invoice number"), max_length=300, blank=True, null=True,
+                                  validators=[alphanumeric('Invoice number')])
+    date = models.DateField(_("date"))
+    amount = models.DecimalField(_("amount"), max_digits=10, decimal_places=2)
+    bill = ProcessedImageField(
+        _("Attach Document"),
+        upload_to='profile',
+        format='WEBP',
+        options={'quality': 50}, blank=True, null=True,
+    )
+    note = models.TextField(_("note"), blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Expense")
+        verbose_name_plural = _("Expenses")
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<Expense(name={self.name})>"
