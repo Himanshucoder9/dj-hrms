eb297dce7fcc8c546abd9595b8f4c4297e45aee8
Prajwal <prajwalkamde264@gmail.com>
2024-06-28 18:40:21 +0530

    Add document, social media and bank details models

--- Employee/models.py
+++ Employee/models.py
@@ -8,7 +8,7 @@ from imagekit.models import ProcessedImageField
 from Master.uploader import staff_directory_path
 
 
-class Education(models.Model):
+class Education(TimeStamp):
     EDUCATION_CHOICES = (
         ('10th', '10th'),
         ('12th', '12th'),
@@ -30,7 +30,7 @@ class Education(models.Model):
         verbose_name_plural = 'Educations'
 
 
-class Document(models.Model):
+class Document(TimeStamp):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name=_('Company'))
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE, verbose_name=_('Staff'))
     aadhar_no = models.CharField(_("Aadhar Number"), max_length=12, null=True,
@@ -43,9 +43,97 @@ class Document(models.Model):
         verbose_name=_("Aadhar Document"),
         upload_to=staff_directory_path,
         validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx', 'pdf', 'png', 'jpg', 'jpeg', 'webp'])],
-        help_text="Upload document..", )
+        help_text="Upload Aadhar card..", )
+    pan_document = models.FileField(
+        verbose_name=_("Pan Document"),
+        upload_to=staff_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx', 'pdf', 'png', 'jpg', 'jpeg', 'webp'])],
+        help_text="Upload Pan card..", )
+    resume = models.FileField(
+        verbose_name=_("Resume"),
+        upload_to=staff_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx', 'pdf', 'png', 'jpg', 'jpeg', 'webp'])],
+        help_text="Upload Resume..",
+        blank=True, null=True)
+    offer_letter = models.FileField(
+        verbose_name=_("Offer Letter"),
+        upload_to=staff_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx', 'pdf', 'png', 'jpg', 'jpeg', 'webp'])],
+        help_text="Upload Offer Letter..",
+        blank=True, null=True)
+    joining_letter = models.FileField(
+        verbose_name=_("Joining Letter"),
+        upload_to=staff_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx', 'pdf', 'png', 'jpg', 'jpeg', 'webp'])],
+        help_text="Upload Joining Letter..",
+        blank=True, null=True)
+    resignation_letter = models.FileField(
+        verbose_name=_("Resignation Letter"),
+        upload_to=staff_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx', 'pdf', 'png', 'jpg', 'jpeg', 'webp'])],
+        help_text="Upload Resignation Letter..",
+        blank=True, null=True)
+    other_document = models.FileField(
+        verbose_name=_("Other Document"),
+        upload_to=staff_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx', 'pdf', 'png', 'jpg', 'jpeg', 'webp'])],
+        help_text="Upload Other Document..",
+        blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"{self.staff.first_name}"
+
+    def __repr__(self):
+        return f"<Document(staff={self.staff})>"
 
     class Meta:
         verbose_name = 'Document'
         verbose_name_plural = 'Documents'
 
+
+class BankDetail(TimeStamp):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name=_('Company'))
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE, verbose_name=_('Staff'))
+    account_holder_name = models.CharField(_("Account Holder Name"), max_length=255)
+    account_no = models.IntegerField(_("Account Number"))
+    bank_name = models.CharField(_("Bank Name"), max_length=255)
+    ifsc_code = models.CharField(_("IFSC Code"), max_length=255)
+    bank_branch = models.CharField(_("Bank Branch"), max_length=255)
+    passbook = models.FileField(
+        verbose_name=_("Passbook"),
+        upload_to=staff_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=['pdf', 'png', 'jpg', 'jpeg', 'webp'])],
+        blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"{self.staff.first_name}"
+
+    def __repr__(self):
+        return f"<BankDetail(staff={self.staff})>"
+
+    class Meta:
+        verbose_name = 'Bank Detail'
+        verbose_name_plural = 'Bank Details'
+
+
+class SocialMedia(TimeStamp):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name=_('Company'))
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE, verbose_name=_('Staff'))
+    linkedin = models.URLField(_("LinkedIn Link"), blank=True, null=True)
+    twitter = models.URLField(_(" Twitter Link"), blank=True, null=True)
+    facebook = models.URLField(_("Facebook Link"), blank=True, null=True)
+    instagram = models.URLField(_("Instagram Link"), blank=True, null=True)
+    thread = models.URLField(_("Thread Link"), blank=True, null=True)
+    github = models.URLField(_("Github Link"), blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.staff.first_name}"
+
+    def __repr__(self):
+        return f"<SocialMedia(staff={self.staff})>"
+
+    class Meta:
+        verbose_name = 'Social Media'
+        verbose_name_plural = 'Social Media'
